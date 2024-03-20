6eb2e946d022e31714a4cc5ad619ae5507c16e48
Ben Wehrend <benwehrend@gmail.com>
2024-03-20 13:01:34 -0700

    adds first couple of tests before switching branches

---
+++ src/card.js
@@ -0,0 +1,22 @@
+const createCard = (id, question, answers, correctAnswer) => {
+    return {
+        id: id,
+        question: question,
+        answers: answers,
+        correctAnswer: correctAnswer
+    }
+}
+
+const evaluateGuess = (guess, card) => {
+    if (guess === card.correctAnswer) {
+        return 'Correct!'
+    } else {
+        return "Incorrect!"
+    }
+}
+
+module.exports = {
+    createCard,
+    evaluateGuess,
+    // createDeck
+}
--- test/card-test.js
+++ test/card-test.js
@@ -1,14 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard } = require('../src/card');
+const { createCard, evaluateGuess } = require('../src/card');
 
 describe('card', function() {
   it.skip('should be a function', function() {
     expect(createCard).to.be.a('function');
   });
 
-  it.skip('should create a card and its properties', function() {
+  it('should create a card and its properties', function() {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     
     expect(card.id).to.equal(1);
@@ -17,3 +17,31 @@ describe('card', function() {
     expect(card.correctAnswer).to.equal('object');
   });  
 });
+
+describe('evaluateGuess', function() {
+  it.skip('should return "Correct!" for right answers', function() {
+    const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const guess = 'object';
+    const result = evaluateGuess(guess, card);
+
+    expect(result).to.be.equal('Correct!');
+  });
+
+  it.skip('should return "Incorrect!" for wrong answers', function() {
+    const card = createCard(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const guess = 'function';
+    const result = evaluateGuess(guess, card);
+
+   expect(result).to.be.equal('Incorrect!');
+  });
+});
+
+describe('createDeck', function() {
+  it.skip('should create a deck of cards', function() {
+    const deck = createDeck([card1, card2, card3])
+  })
+
+  it.skip('should know how many cards are in the deck', function() {
+
+  })
+})
